9cc71268c751274b3b5af9eb10558affdd0e6b31
shanzaboy <shanza1974@gmail.com>
2021-04-14 20:01:42 +0100

    homework

--- week_03/day_02/lab_homework.sql
+++ week_03/day_02/lab_homework.sql
@@ -1,197 +1,247 @@
---1 MVP
+/*Question 1.
+Are there any pay_details records lacking both a local_account_no and iban number?*/
 
-
---Question 1.
-/*(a). Find the first name, last name and team name of employees who are members of teams.
-
-Hint
-We only want employees who are also in the teams table. So which type of join should we use*/
-
-SELECT 
-	e.first_name,
-	e.last_name,
-	t.name as team_name
-FROM 
-	employees AS e
-INNER JOIN teams AS t
-ON e.team_id = t.id;
+SELECT
+*
+FROM pay_details
+WHERE local_account_no IS NULL AND iban IS NULL;
 
 
 
-/*(b). Find the first name, last name and team name of employees who are members of teams and are enrolled in the pension scheme.*/
-
-SELECT 
-	e.first_name,
-	e.last_name,
-	t.name as team_name
-FROM 
-	employees AS e
-INNER JOIN teams AS t
-ON e.team_id = t.id
-WHERE pension_enrol = TRUE;
-
-
-
-/*(c). Find the first name, last name and team name of employees who are members of teams, where their team has a charge cost greater than 80.
-
-Hint
-charge_cost may be the wrong type to compare with value 80. Can you find a way to convert it without changing the database?*/
-
-
-SELECT 
-	e.first_name,
-	e.last_name,
-	t.name as team_name 
-FROM 
-	employees AS e
-INNER JOIN teams AS t
-ON e.team_id = t.id
-WHERE CAST(t.charge_cost AS INTEGER) > 80;
 
 
 /*Question 2.
-(a). Get a table of all employees details, together with their local_account_no and local_sort_code, if they have them.
-
-Hints
-local_account_no and local_sort_code are fields in pay_details, and employee details are held in employees, so this query requires a JOIN.
-
-What sort of JOIN is needed if we want details of all employees, even if they don’t have stored local_account_no and local_sort_code?*/
-
-
-
-
-
-/*(b). Amend your query above to also return the name of the team that each employee belongs to.*/
-
+Get a table of employees first_name, last_name and country, ordered alphabetically first by country and then by last_name (put any NULLs last).*/
 
 SELECT
-e.*,
-pd.local_account_no,
-pd.local_sort_code,
-t.name AS team_name
-FROM (employees AS e
-LEFT JOIN pay_details AS pd
-ON e.id = pd.id)
-INNER JOIN teams AS t
-ON e.team_id = t.id;
-
-/*Hint
-The name of the team is in the teams table, so we will need to do another join.
+last_name,
+country,
+first_name
+FROM employees
+ORDER BY country, last_name;
 
 
-
-*/
 
 /*Question 3.
-(a). Make a table, which has each employee id along with the team that employee belongs to.
+Find the details of the top ten highest paid employees in the corporation.*/
+SELECT
+last_name,
+salary
+FROM employees
+ORDER BY salary DESC NULLS LAST
+LIMIT 10;
 
 
 
-(b). Breakdown the number of employees in each of the teams.
 
-Hint
-You will need to add a group by to the table you created above.
-
-
-(c). Order the table above by so that the teams with the least employees come first.*/
-
-
-SELECT 
-COUNT(t.id) AS no_id,
-t.name AS team_name
-FROM employees AS e
-LEFT JOIN teams AS t
-ON e.team_id = t.id
-GROUP BY t.id;
-
-SELECT 
-COUNT(t.id) AS no_id,
-t.name AS team_name
-FROM employees AS e
-LEFT JOIN teams AS t
-ON e.team_id = t.id
-GROUP BY t.id
-ORDER BY no_id ASC;
 
 /*Question 4.
-(a). Create a table with the team id, team name and the count of the number of employees in each team.*/
-
-SELECT 
-t.id,
-COUNT(t.id) AS no_id,
-t.name AS team_name
-FROM employees AS e
-LEFT JOIN teams AS t
-ON e.team_id = t.id
-GROUP BY t.id
-;
-
-/*(b). The total_day_charge of a team is defined as the charge_cost of the team multiplied by the number of employees in the team. Calculate the total_day_charge for each team.
-
-Hint
-If you GROUP BY teams.id, because it’s the primary key, you can SELECT any other column of teams that you want (this is an exception 
-to the rule that normally you can only SELECT a column that you GROUP BY).
-*/
+Find the first_name, last_name and salary of the lowest paid employee in Hungary*/
+SELECT
+first_name,
+last_name,
+salary
+FROM employees
+WHERE country = 'Hungary'
+ORDER BY salary ASC NULLS LAST;
 
 
-SELECT 
-	t.id AS team_id,
-	t.name AS team_name,
-	COUNT(t.id) AS no_members,
-	t.charge_cost AS charge_cost,
-	COUNT(t.id) * CAST(t.charge_cost AS INTEGER) AS final_cost 
-FROM 
-	employees AS emp
-LEFT JOIN teams AS t
-ON emp.team_id = t.id
-GROUP BY t.id
-ORDER BY no_members;
-
-/*(c). How would you amend your query from above to show only those teams with a total_day_charge greater than 5000?*/
-
-SELECT *
-FROM (
-	SELECT 
-		t.id AS team_id,
-		t.name AS team_name,
-		COUNT(t.id) AS no_members,
-		t.charge_cost AS charge_cost,
-		COUNT(t.id) * CAST(t.charge_cost AS INTEGER) AS final_cost 
-	FROM 
-		employees AS emp
-	LEFT JOIN teams AS t
-	ON emp.team_id = t.id
-	GROUP BY t.id
-	ORDER BY no_members) AS charge_table
-WHERE charge_table.final_cost > 5000;
 
 /*Question 5.
-How many of the employees serve on one or more committees?
+Find all the details of any employees with a ‘yahoo’ email address?*/
 
-
-Hints
-All of the details of membership of committees is held in a single table: employees_committees, so this doesn’t require a join.
-
-Some employees may serve in multiple committees. Can you find the number of distinct employees who serve? [Extra hint - do some research on the DISTINCT() function].*/
-
-
-SELECT 
-COUNT (DISTINCT employee_id)
-FROM employees_committees;
-
-
-
-
-
+SELECT
+first_name,
+last_name
+FROM employees
+WHERE email LIKE '%@yahoo%';
 
 
 
 /*Question 6.
-How many of the employees do not serve on a committee?
+Obtain a count by department of the employees who started work with the corporation in 2003.*/
+
+SELECT
+department,
+COUNT(id)
+FROM employees
+WHERE start_date >= '2003-01-01'
+GROUP BY department;
+
+
+/*Question 7.
+Obtain a table showing department, fte_hours and the number of employees in each department who work each fte_hours pattern. 
+Order the table alphabetically by department, and then in ascending order of fte_hours.*/ 
+/*Hint
+You need to GROUP BY two columns here.*/
+
+SELECT
+	department,
+	fte_hours,
+	COUNT(id)
+FROM employees
+GROUP BY department, fte_hours;
+
+
+/*Question 8.
+Provide a breakdown of the numbers of employees enrolled, not enrolled, and with unknown enrollment status in the corporation pension scheme.*/
+
+SELECT 
+pension_enrol,
+COUNT(id)
+FROM employees
+GROUP by pension_enrol;
+
+
+/*Question 9.
+What is the maximum salary among those employees in the ‘Engineering’ department who work 1.0 full-time equivalent hours (fte_hours)?*/
+
+SELECT
+salary
+FROM employees
+WHERE fte_hours = 1 AND department = 'Engineering'
+ORDER BY salary DESC
+LIMIT 1;
+
+
+
+/*Question 10.
+Get a table of country, number of employees in that country, and the average salary of employees in that country for any 
+countries in which more than 30 employees are based. Order the table by average salary descending.
+
+Hints
+A HAVING clause is needed to filter using an aggregate function.
+
+You can pass a column alias to ORDER BY.*/
+
+SELECT
+country,
+COUNT(id),
+ROUND(AVG(salary)) AS avg_salary
+FROM employees
+GROUP BY country
+HAVING COUNT(id) >30
+ORDER BY avg_salary DESC;
+
+
+/*Question 11.
+Return a table containing each employees first_name, last_name, full-time equivalent hours (fte_hours), salary, 
+and a new column effective_yearly_salary which should contain fte_hours multiplied by salary.*/
+
+SELECT
+first_name,
+last_name,
+fte_hours,
+salary,
+ROUND(fte_hours * salary) AS effective_yearly_salary
+FROM employees;
+
+
+
+
+/*Question 12.
+Find the first name and last name of all employees who lack a local_tax_code.
+
+
+Hint
+local_tax_code is a field in table pay_details, and first_name and last_name are fields in table employees, 
+so this query requires some sort of JOIN!*/
+
+
+SELECT
+	e.first_name,
+	e.last_name,
+	pd.local_tax_code
+FROM employees AS e
+FULL OUTER JOIN pay_details AS pd
+ON e.id = pd.id
+WHERE pd.local_tax_code IS NULL;
+
+
+
+
+/*Question 13.
+The expected_profit of an employee is defined as (48 * 35 * charge_cost - salary) * fte_hours, where charge_cost depends upon the team
+to which the employee belongs. Get a table showing expected_profit for each employee.
 
 
 Hints
-This requires joining over only two tables
+charge_cost is in teams, while salary and fte_hours are in employees, so a join will be necessary
 
-Could you use a join and find rows without a match in the join?*/
+You will need to change the type of charge_cost in order to perform the calculation*/
 
+SELECT
+	e.first_name,
+	e.last_name,
+	ROUND((48 * 35 * (CAST(t.charge_cost AS INT)) - e.salary) * e.fte_hours) AS expected_profit
+FROM employees AS e
+LEFT JOIN teams AS t
+ON e.team_id = t.id;
+
+
+
+/*Question 14.
+Obtain a table showing any departments in which there are two or more employees lacking a stored first name. 
+Order the table in descending order of the number of employees lacking a first name, and then in alphabetical order by department.*/
+
+
+
+SELECT
+department
+FROM employees
+WHERE first_name IS NULL
+GROUP BY department
+HAVING COUNT(id) >= '2'
+ORDER BY department DESC;
+
+
+/*Question 15.
+[Bit Tougher] Return a table of those employee first_names shared by more than one employee, together with a count of the 
+number of times each first_name occurs. Omit employees without a stored first_name from the table. 
+Order the table descending by count, and then alphabetically by first_name.*/
+
+SELECT 
+first_name,
+COUNT(id) AS count
+FROM employees
+WHERE first_name IS NOT NULL
+GROUP BY first_name
+HAVING COUNT(id) > 1
+ORDER BY count(id) DESC, first_name;
+
+
+
+
+/*Question 16.
+[Tough] Find the proportion of employees in each department who are grade 1.
+
+Hints
+Think of the desired proportion for a given department as the number of employees in that department who are grade 1, divided by the total number of employees in that department.
+
+
+You can write an expression in a SELECT statement, e.g. grade = 1. This would result in BOOLEAN values.
+
+
+If you could convert BOOLEAN to INTEGER 1 and 0, you could sum them. The CAST() function lets you convert data types.
+
+
+In SQL, an INTEGER divided by an INTEGER yields an INTEGER. To get a REAL value, you need to convert the top, bottom or both sides of the division to REAL.*/
+
+WITH total_emp AS(
+	SELECT
+		department,
+		COUNT(id) AS total_emp_count
+	FROM employees
+	GROUP BY department
+)
+SELECT
+	e.department,
+	COUNT(id) AS number_employees,
+	e.grade,
+	te.total_emp_count,
+	CAST(COUNT(id) AS REAL)/CAST(te.total_emp_count AS REAL) AS proportion
+FROM employees AS e
+INNER JOIN total_emp as te
+ON e.department = te.department
+WHERE grade =1
+GROUP BY e.department, grade, te.total_emp_count;
