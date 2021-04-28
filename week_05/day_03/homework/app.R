4c9e3272f31c08db530e20702acff40530bdd020
shanzaboy <shanza1974@gmail.com>
2021-04-28 21:19:06 +0100

    homework

---
+++ week_05/day_03/homework/app.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(tidyverse)
+library(shinythemes)
+library(CodeClanData)
+all_teams <- unique(olympics_overall_medals$team)
+ui <- fluidPage(
+    theme = shinytheme("darkly"),
+    tags$i(titlePanel("Olympic Medals")),
+    
+    plotOutput("medal_plot"),
+    
+    
+    
+    tabsetPanel(
+        tabPanel(
+            fluidRow(
+            column(9, 
+               radioButtons("season",
+                            "Summer or Winter Olympics?",
+                            choices = c("Summer", "Winter")
+               )
+        ),
+        column(3,
+               selectInput("team",
+                          tags$i("Which Team?"),
+                           choices = all_teams
+               )   
+        )
+        
+    ),
+    tabPanel("Olympics Website",
+             fluidRow(
+                 column(12,tags$i(tags$a("The Olympics website", 
+                                         href = "https://www.olympic.org"
+                                         )
+                                 )
+                )
+    
+             )
+    )
+    ))
+)
+server <- function(input, output) {
+    output$medal_plot <- renderPlot({
+        olympics_overall_medals %>%
+            filter(team == input$team) %>%
+            filter(season == input$season) %>%
+            ggplot() +
+            aes(x = medal, y = count, fill = medal) +
+            geom_col()
+    })
+}
+shinyApp(ui = ui, server = server)
